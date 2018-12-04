cd935775645e6612baf77d9d7a9c8e78ea9d1a85
kplane20 <kep2671@rit.edu>
2018-12-04 14:35:04 -0500

    practical practice

--- week14b/fall2018sample-3/fall2018sample.js
+++ week14b/fall2018sample-3/fall2018sample.js
@@ -1,23 +1,26 @@
 
 
 /* Don't use <script> tags in a linked js file! */
+ 
+$(".menuitem").click(function(){ //on click of my menu option
+    $(".submenu").slideToggle(500); //show or hide the submenu
+});
 
-$(".menuitem").click(function(){
-        $(".submenu").slideToggle(500);
-    });
+$(".menuitem1").click(function(){ //on click of my menu option
+    $(".submenu1").slideToggle(500); //show or hide the submenu
+});
 
-    $(".menuitem1").click(function(){ //on click of my menu option
-        $(".submenu1").slideToggle(500); //show or hide the submenu
-    });
-    
+
+
+
 
 //loading in the content, individual pages
 
-// 'use strict';
-let verse = ("content1.txt")  // sets default verse element
+'use strict';
+let verse = ("content1.txt")  // sets default verse element
 
 $("#choose-content").val(verse); // changes menu option to default
-$("#content").load(verse);   // retrieves only default element
+$("#content").load(verse);   // retrieves only default element
 
 $("#choose-content").change(function() { //on change, when a new menu item is selected do this
 verse = $(this).val(); //the verse which is my variable for the content information is set to the new value of the selected menu item.
