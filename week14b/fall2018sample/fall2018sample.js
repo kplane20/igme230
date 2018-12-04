1b90e0a708cbbb97093f0e0b33f6c6ba7eb95168
kplane20 <kep2671@rit.edu>
2018-12-04 13:16:18 -0500

    sumbit4

---
+++ week14b/fall2018sample/fall2018sample.js
@@ -0,0 +1,21 @@
+
+
+/* Don't use <script> tags in a linked js file! */
+
+$(".menuitem").click(function(){
+        $(".submenu").slideToggle(500);
+    });
+
+
+//loading in the content, individual pages
+
+// 'use strict';
+let verse = ("content1.txt")  // sets default verse element
+
+$("#choose-content").val(verse); // changes menu option to default
+$("#content").load(verse);   // retrieves only default element
+
+$("#choose-content").change(function() { //on change, when a new menu item is selected do this
+verse = $(this).val(); //the verse which is my variable for the content information is set to the new value of the selected menu item.
+$("#content").load(verse); //upload the new verse and replace the old one
+});
