e99262f6fd98280158bbf6da624e52c0e23f4b83
kplane20 <kep2671@rit.edu>
2018-12-13 11:40:59 -0500

    testing final practical makeup

---
+++ makeup4/fall2018finalmakeup.js
@@ -0,0 +1,41 @@
+
+//extra credit
+let count = 0;
+$("#increment").click(function(){ //on click of my menu option
+    count= count + 1; //count++;
+    $("#currentcount").html(count);
+   
+});
+
+// menu hover
+$(".menu").hover(function(){
+    $(this).css("background-color", "lightpink"); 
+}, function(){
+    $(this).css("background-color", "lightpink");  
+});
+
+$("li").hover(function(){
+    $(this).css("background-color", "lightpink"); 
+    $(this).css('color', 'magenta');
+}, 
+function(){
+    $(this).css("background-color", "#ccc");  
+});
+
+// menu drop down
+$(".menu").click(function() {
+    $(this).next('.submenu').slideToggle('slow');
+});
+
+
+// article/story 
+//loadds the fileinto article section on the grid
+$("story").load("story0.txt");
+
+// $("input[name='content']").click(function(){
+
+//     let newarticle= this.value;
+
+//     $("story").load(newarticle);
+// });
+
