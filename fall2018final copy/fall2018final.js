c96ea4f115d7f33dc7043d50c76ca7d1198555fa
kplane20 <kep2671@rit.edu>
2018-12-12 23:17:40 -0500

    study grid test

--- fall2018final copy/fall2018final.js
+++ fall2018final copy/fall2018final.js
@@ -4,27 +4,81 @@ $(".mainmenu").click(function() {
     $(this).next('.submenu').slideToggle('slow');
 });
 
+
 // radio button code
 
-let verse = ("article0.txt")  // sets default verse element
 
-$(".choose-content").val(verse); // changes menu option to default
-$("#content").load(verse);   // retrieves only default element
+//loadds the fileinto article section on the grid
+// $("mainmenu").load(".submenu");
+
+$("input[name='content']").click(function(){
+
+    let showmenu= this.value;
+
+    $("mainmenu").load(showmenu);
+});
+
+//loadds the fileinto article section on the grid
+// $("article").load("article0.txt");
+
+// $("input[name='content']").click(function(){
+
+//     let newarticle= this.value;
+
+//     $("article").load(newarticle);
+// });
 
 
-$("#choose-content").click(function() { //on change, when a new menu item is selected do this
-    verse = ("article1.txt") //the verse which is my variable for the content information is set to the new value of the selected menu item.
-        
-    $("#content").load(verse); //upload the new verse and replace the old one
-    });
+
+//menu option
+
+
+//shows and hides nav
+$("#hide").click(function () {
+    $("nav").animate({ left: '-100' }, "slow");
+
+});
+
+$("#show").click(function () {
+    $("nav").animate({ left: '0px' }, "slow");
+
+});
+
+
+//Changed background if you hover 
+$(".mainmenu").hover(function(){
+    $(this).css("background-color", "yellow"); 
+}, function(){
+    $(this).css("background-color", "#ccc");  
+});
+
+$("li").hover(function(){
+    $(this).css("background-color", "orange"); 
+}, function(){
+    $(this).css("background-color", "#86af49");  
+});
+
+
+
+
+    
+    // let verse = ("article0.txt")  // sets default verse element
+
+// $(".choose-content").val(verse); // changes menu option to default
+// $("#content").load(verse);   // retrieves only default element
+
+
+// $("#choose-content").click(function() { //on change, when a new menu item is selected do this
+//     verse = ("article1.txt") //the verse which is my variable for the content information is set to the new value of the selected menu item.
+//         
+//     $("#content").load(verse); //upload the new verse and replace the old one
+//     });
     
     
     
     
-    $("#choose-contentn").click(function() { //on change, when a new menu item is selected do this
-    verse = ("article2.txt") //the verse which is my variable for the content information is set to the new value of the selected menu item.
-        
-    $("#content").load(verse); //upload the new verse and replace the old one
-    });
-    
-    
+//     $("#choose-contentn").click(function() { //on change, when a new menu item is selected do this
+//     verse = ("article2.txt") //the verse which is my variable for the content information is set to the new value of the selected menu item.
+//         
+//     $("#content").load(verse); //upload the new verse and replace the old one
+//     });
